e4ab06ecdb7e7f5fcbbe466ac1188d884b04c827
EhingeLeandro <ehinger.leandro@hotmail.com>
2024-08-27 15:26:44 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-// import { useState } from 'react'
 import './App.css';
 import Cards from "./components/Cards.jsx"
 
 function App() {
  
   return (
-    <div className="App d-flex justify-content-center align-items-center " >
+    <div className="App" >
       <Cards/>
     </div>
   )
